322a86df93640c36b11997e149eb9837b93e8e39
eawebmaster20 <eaweb20@gmail.com>
2024-06-13 05:58:18 -0700

    feat: added a functionality to modularize the 'li' element

--- main.js
+++ main.js
@@ -17,3 +17,20 @@ const addTodo = ()=>{
     clearInput()
     console.log(newLi);
 }
+
+const updateLi = (title, dateDue, description)=>{
+    return `
+        <div class="d-flex w-100 align-items-center mb-2 todo-item card">
+            <div class="row row-cols-4 w-100 ">
+                <span class="fw-bold">${title.replace(title.charAt(0),title.charAt(0).toUpperCase())}</span>
+                <span class="small text-secondary">${dateDue}</span>
+                <span class="small task-desc text-secondary">${description}</span>
+                <span class="d-flex options">
+                    <i class="ms-auto complete" onClick= "toggleComplete(this)"><input type="checkbox"/></i>
+                    <i class="ms-auto editbtn" onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#exampleModal"><img class="btn-icon" src="./assets/edit.svg" alt="" srcset=""></i>
+                    <i class="ms-2 editbtn" onClick ="deleteTask(this)"><img class="btn-icon" src="./assets/delete.svg" alt="" srcset=""></i>
+                </span>
+            </div>
+            
+        </div>`
+}
